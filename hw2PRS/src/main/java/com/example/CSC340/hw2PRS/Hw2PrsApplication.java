532da84d97b0727a897ab7bc49c232852c03a11f
Priyanka Shah <priyanka2003shah@gmail.com>
2023-09-07 12:08:07 -0400

    add initial files

---
+++ hw2PRS/src/main/java/com/example/CSC340/hw2PRS/Hw2PrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSC340.hw2PRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw2PrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw2PrsApplication.class, args);
+	}
+
+}
---
+++ hw2PRS/src/test/java/com/example/CSC340/hw2PRS/Hw2PrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSC340.hw2PRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw2PrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
